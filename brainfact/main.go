9b68b951cf266cee64cc5a08272b03c1469dccac
ever-eduardo <106024807+ever-eduardo@users.noreply.github.com>
2023-06-18 23:51:13 -0500

    Add examples folder

--- brainfact/main.go
+++ brainfact/main.go
@@ -4,24 +4,42 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"unicode/utf8"
 
 	brainfact "github.com/ever-eduardo/brain-fact"
 )
 
 const BFFileExtension string = ".bf"
-const Welcome string = "Welcome to Brainfact REPL! 🌎"
+const REPLWelcome string = "Welcome to Brainfact REPL! 🌎"
+const Welcome string = "Brainfact! 🌎"
+const Success string = "Your script was successfully compiled and run."
 const Description string = "A brainfuck interpreter written in Go."
 const Help string = "Type .help for assistance"
 
 func main() {
 	clearScreen()
 	if len(os.Args) > 1 {
-
+		scriptIndex := 1
+		if strings.HasSuffix(os.Args[scriptIndex], BFFileExtension) {
+			if bin, err := os.ReadFile(os.Args[scriptIndex]); err == nil && utf8.Valid(bin) {
+				code := string(bin)
+				if err := brainfact.Run(code); err != nil {
+					fmt.Printf("\n\n\n Error: %s\n\n\n", err)
+				} else {
+					fmt.Printf("\n\n\n   %s\n   %s\n\n\n", Welcome, Success)
+				}
+			} else if err != nil {
+				fmt.Printf("\n\nError reading file '%v'\n%v\n\n\n", os.Args[scriptIndex], err)
+			} else {
+				fmt.Printf("\n\nThe file '%v' does not contain a valid utf-8 sequence of bytes\n\n\n", os.Args[scriptIndex])
+			}
+		}
 	} else {
 		var code string
 		running := true
 		scanner := bufio.NewScanner(os.Stdin)
-		fmt.Printf("\n\n\n   %s\n   %s\n   %s\n\n\n", Welcome, Description, Help)
+		fmt.Printf("\n\n\n   %s\n   %s\n   %s\n\n\n", REPLWelcome, Description, Help)
 		for running {
 			fmt.Printf("%v", brainfact.Prompt)
 			if scanner.Scan() {
